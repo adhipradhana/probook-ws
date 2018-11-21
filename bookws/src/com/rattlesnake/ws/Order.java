0f087d3dbc2de5c38c841d2786ea517fe8a8bb30
adhipradhana <adhi.pradana.pradana@gmail.com>
2018-11-21 19:51:39 +0700

    create book xml type

--- bookws/src/com/rattlesnake/ws/Order.java
+++ bookws/src/com/rattlesnake/ws/Order.java
@@ -1,11 +1,15 @@
 package com.rattlesnake.ws;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
 
-@WebService(endpointInterface="com.rattlesnake.ws.OrderInterface")
-public class Order implements OrderInterface {
-	
-	@Override
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class Order {
+    
+    @WebMethod
 	public int getOrderNumber() {
         return 1;
     }
