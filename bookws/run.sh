0374f6c5c41fe5c810cdd8b76bf3815d5b0e0b4d
adhipradhana <adhi.pradana.pradana@gmail.com>
2018-11-22 12:10:35 +0700

    modify compilation of run.sh

--- bookws/run.sh
+++ bookws/run.sh
@@ -4,8 +4,7 @@
 #cp target/bookws-1.0.war /usr/local/Cellar/tomcat/9.0.12/libexec/webapps/bookws.war
 #brew services restart tomcat
 
-javac -d build src/com/rattlesnake/ws/*.java src/com/rattlesnake/endpoint/*.java
+javac -d build src/com/rattlesnake/methods/*.java src/com/rattlesnake/ws/*.java src/com/rattlesnake/endpoint/*.java
 cd build
-echo "Running on port 9999"
 java com.rattlesnake.endpoint.BookWSPublisher
 
