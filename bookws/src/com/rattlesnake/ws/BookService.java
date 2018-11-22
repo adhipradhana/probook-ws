e380cc95737b0d347a95541871f33992e7992e30
adhipradhana <adhi.pradana.pradana@gmail.com>
2018-11-22 23:05:00 +0700

    implementing get book detail

--- bookws/src/com/rattlesnake/ws/BookService.java
+++ bookws/src/com/rattlesnake/ws/BookService.java
@@ -14,11 +14,11 @@ import com.rattlesnake.models.Book;
 public class BookService {
 
     private final String BOOK_API_KEY = "AIzaSyAmKiuIzrY3aUGm6nh5MjVq7gaio0xobv8";
-    private final String BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:";
+    private final String BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 
     @WebMethod
     public Book[] searchBook(String searchTitle) {
-        String targetURL = BASE_URL + searchTitle + "&key=" + BOOK_API_KEY;
+        String targetURL = BASE_URL + "?q=intitle:" + searchTitle + "&key=" + BOOK_API_KEY;
 
         // return null if error
         String response = HTTPMethod.executeGet(targetURL);
@@ -74,6 +74,52 @@ public class BookService {
         }
 
         return bookList;
+    }
+
+    @WebMethod
+    public Book getBookDetail(String id) {
+        String targetURL = BASE_URL + "/" + id + "?key=" + BOOK_API_KEY;
+
+        // return null if error
+        String response = HTTPMethod.executeGet(targetURL);
+        if (response == null) {
+            return new Book();
+        }
+
+        // parse json object
+        JSONObject item = new JSONObject(response);
+
+        // construct book
+        Book book = new Book();
+
+        // assign id
+        book.setId(id);
+
+        // assign title
+        String title = item.getJSONObject("volumeInfo").getString("title");
+        book.setTitle(title);
+
+        // assign author
+        String author = item.getJSONObject("volumeInfo").getJSONArray("authors").getString(0);
+        book.setAuthor(author);
+
+        // assign image
+        String image = item.getJSONObject("volumeInfo").getJSONObject("imageLinks").getString("thumbnail");
+        book.setImage(image);
+
+        // assign description
+        String description = item.getJSONObject("volumeInfo").getString("description");
+        book.setDescription(description);
+
+        // assign rating
+        double averageRating = item.getJSONObject("volumeInfo").getDouble("averageRating");
+        book.setRating(averageRating);
+
+        // assign price
+        long price = item.getJSONObject("saleInfo").getJSONObject("retailPrice").getLong("amount");
+        book.setPrice(price);
+
+        return book;
     }
 
 }
--- bookws/src/test/test.php
+++ bookws/src/test/test.php
@@ -3,6 +3,7 @@
 $client = new SoapClient('http://localhost:3000/service/book?wsdl', array('cache_wsdl' => WSDL_CACHE_NONE) );
 
 var_dump($client->__getFunctions());
-var_dump($client->searchBook("earth"));
+var_dump($client->searchBook("earth+mankind"));
+var_dump($client->getBookDetail("s5xrAwAAQBAJ"));
 
 ?>
